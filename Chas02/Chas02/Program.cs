5371474a68cc2749fb1f2aa5d42a614b817b249d
DimitarKastratovikj <DimitarKastratovikj@users.noreply.github.com>
2020-03-05 20:48:26 +0100

    kraj chas

--- Chas02/Chas02/Program.cs
+++ Chas02/Chas02/Program.cs
@@ -6,14 +6,70 @@ namespace Chas02
     {
         static void Main(string[] args)
         {
-            int number = 10;
+            #region 02
+            //int number = 10;
 
-            number = 20;
+            //bool answer = true;
 
-            asd
+            //char bukva = 'a';
 
-            Console.WriteLine("Hello World!");
+            //string tekst = "opa cupa";
 
+            //var firstName = "Dime";
+
+            //float floatNumber = 10.22f;
+            //double doubleNumber = 10.22;
+            //decimal decimalNumber = 10.22m;
+
+
+
+            //Console.WriteLine(int.MaxValue);
+
+            //byte od 0 do 225 moze da primi vrednost
+
+            //byte bytenumber = 255;
+            //byte byteee = 27;
+
+            double doubleNumber1 = 155.22;
+            double doubleNumber2 = 12.22;
+            double doubleNumber3 = doubleNumber1/ doubleNumber2;
+
+            Console.WriteLine(doubleNumber3);
+
+            int number1 = 1211;
+            int number2 = 20;
+            int number3 = number1/ number2;
+            Console.WriteLine(number3);
+            #endregion
+
+            #region 03
+            //var broShoKegovneses = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(broShoKegovneses + 22);
+
+
+            //Console.WriteLine();
+            //Console.ReadLine();
+
+            //string input = "10";
+            //int number11 = (string) input;
+
+
+
+            //int number = 255;
+            //byte antNumber = (byte) number;
+
+
+            //int.parse
+            string input2 = "123";
+            int numbervalue =int.Parse( input2);
+
+
+            int val2 = Convert.ToInt32(input2);
+
+
+
+#endregion
+            Console.ReadLine();
         }
     }
 }
---
+++ Chas02/dataConversion/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace dataConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //enter number from console
+
+            //print number
+
+
+            //Console.WriteLine("vnesi broj");
+
+            //var input = Console.ReadLine();
+            //int number;
+
+            //bool result = int.TryParse(input, out number);
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(result);
+
+            //string input3 = ""
+
+            Console.WriteLine("vnesi broj");
+
+            var input = Console.ReadLine();
+
+            int number1;
+            int number2;
+
+            bool reulst = int.TryParse(input, out number1);
+            if (reulst)
+            {
+                number2 = 100 / number1;
+                Console.WriteLine(number2);
+            }
+            else
+            {
+                Console.WriteLine("KE VNESES JA SHO KE TI KAZAM");
+            }
+
+            
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
