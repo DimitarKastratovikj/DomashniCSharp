0db56c0889bdcd55950244dfa61c32c7ca1b714c
DimitarKastratovikj <DimitarKastratovikj@users.noreply.github.com>
2020-03-05 18:03:16 +0100

    asd

--- Chas02/Chas02/Program.cs
+++ Chas02/Chas02/Program.cs
@@ -7,6 +7,10 @@ namespace Chas02
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
         }
     }
 }
