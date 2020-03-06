f3d9f5854ac0dd3aed2aee002d4de625f3003cc9
DimitarKastratovikj <61827749+DimitarKastratovikj@users.noreply.github.com>
2020-03-06 11:37:01 +0100

    dom1

---
+++ domasna01/domasna01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace domasna01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hellouu Driver!");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("  STOP   ");
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("  READY  ");
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("  GO!    ");
+
+            Console.ResetColor();
+            Console.ReadLine();
+            Console.WriteLine("Baj baaaj Driver!");
+            Console.ReadLine();
+        }
+    }
+}
