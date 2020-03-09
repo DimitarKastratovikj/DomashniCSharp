00f03668f127f0b751547e76b21ad223a76b056d
DimitarKastratovikj <61827749+DimitarKastratovikj@users.noreply.github.com>
2020-03-09 15:53:30 +0100

    0903

---
+++ Chas02/VezbiNet01/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace VezbiNet01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 01
+            Console.WriteLine("1.Write a C# Sharp program to print Hello and your name in a separate line.");
+            Console.WriteLine();
+            Console.WriteLine("Hellou:");
+            Console.WriteLine("Alexandra Abramov");
+            #endregion
+
+            #region 02
+            Console.WriteLine("2.Write a C# Sharp program to print the sum of two numbers.");
+            Console.WriteLine(5 + 6);
+            #endregion
+
+            #region 03
+            Console.WriteLine("3. Write a C# Sharp program to print the result of dividing two numbers.");
+            Console.WriteLine(24 / 6);
+            #endregion
+
+            #region 04
+            //4.Write a C# Sharp program to print the result of the specified operations
+            Console.WriteLine("4.Write a C# Sharp program to print the result of the specified operations");
+            Console.WriteLine(-1 + 4 * 6);
+            Console.WriteLine((35 + 5) % 7);
+            Console.WriteLine(14 + -4 * 6 / 11);
+            Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+            #endregion
+
+            #region 05
+            Console.WriteLine("5. Write a C# Sharp program to swap two numbers");
+            int num1, num2, tmp;
+
+            Console.Write("\nInput the First Number : ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\nInput the Second Number : ");
+            num2 = int.Parse(Console.ReadLine());
+
+            tmp = num1;
+            num1 = num2;
+            num2 = tmp;
+
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : " + num1);
+            Console.Write("\nSecond Number : " + num2);
+            Console.WriteLine();
+            Console.WriteLine();
+            #endregion
+
+            #region 06
+            Console.WriteLine("Write a C# Sharp program to print the output of multiplication " +
+                "of three numbers which will be entered by the user");
+            Console.WriteLine();
+            //Test Data:
+            //Input the first number to multiply: 2
+            //Input the second number to multiply: 3
+            //Input the third number to multiply: 6
+            //Expected Output:
+            //2 x 3 x 6 = 36
+            Console.Write("\nInput the 1st Number : ");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the 2nd Number : ");
+            int num4 = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the 3th Number : ");
+            int num5 = int.Parse(Console.ReadLine());
+            Console.Write("\nAfter multiplication : ");
+            Console.WriteLine();
+            Console.WriteLine($"First number to multiply: {num3}");
+            Console.WriteLine($"Second number to multiply: {num4}");
+            Console.WriteLine($"Third number to multiply: {num5}");
+            Console.WriteLine();
+            Console.WriteLine($" Output: {num3 * num4 * num5}");
+            Console.WriteLine();
+            #endregion
+
+            #region 07
+            Console.WriteLine("7. Write a C# Sharp program to print on screen the output of" +
+                " adding, subtracting, multiplying and dividing of two numbers which will be entered by the user.");
+            Console.WriteLine();
+            Console.Write("\nInput the First number : ");
+            int num6 = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the Second number : ");
+            int num7 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"First and Second number adding result is : {num6 + num7} ");
+            Console.WriteLine($"First and Second number subtracting result is : {num6 - num7} ");
+            Console.WriteLine($"First and Second number multiplying result is : {num6 * num7} ");
+            Console.WriteLine($"First and Second number dividing result is : {num6 / num7} ");
+            Console.WriteLine();
+            #endregion
+
+            #region 08
+            Console.WriteLine("Write a C# Sharp program that takes a number as input" +
+                " and print its multiplication table.");
+            //Test Data:
+            //Enter the number: 5
+            //Expected Output:
+            //5 * 0 = 0
+            //5 * 1 = 5
+            //5 * 2 = 10
+            //5 * 3 = 15
+            //....
+            //5 * 10 = 50
+            int x;
+            int result1;
+
+            Console.WriteLine("Enter a number:");
+            x = Convert.ToInt32(Console.ReadLine());
+            result1 = x * 1;
+            Console.WriteLine("The table is : {0} x {1} = {2}", x, 1, result1); 
+
+            // "The table is : {0} x {1} = {2}"                                                                              
+            //ова го праи несто као ареј а пак                                                                               
+            //, x, 1, result1 му става вредности                                                                        
+            // прашањето е зашо нема $ пред наводниците
+
+            result1 = x * 2;
+            Console.WriteLine("             : {0} x {1} = {2}", x, 2, result1);
+            result1 = x * 3;
+            Console.WriteLine("             : {0} x {1} = {2}", x, 3, result1);
+            result1 = x * 4;
+            Console.WriteLine("             : {0} x {1} = {2}", x, 4, result1);
+            result1 = x * 5;
+            Console.WriteLine("             : {0} x {1} = {2}", x, 5, result1);
+            result1 = x * 6;
+            Console.WriteLine("             : {0} x {1} = {2}", x, 6, result1);
+            result1 = x * 7;
+            Console.WriteLine("             : {0} x {1} = {2}", x, 7, result1);
+            result1 = x * 8;
+            Console.WriteLine("             : {0} x {1} = {2}", x, 8, result1);
+            result1 = x * 9;
+            Console.WriteLine("             : {0} x {1} = {2}", x, 9, result1);
+            result1 = x * 10;
+            Console.WriteLine("             : {0} x {1} = {2}", x, 10, result1);
+
+            #endregion
+            #region 09
+
+            #endregion
+            #region 10
+            #endregion
+            #region 11
+            #endregion
+            #region 12
+            #endregion
+            #region 13
+            #endregion
+            #region 14
+            #endregion
+            #region 15
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chas02/vezbiidomasno02/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace vezbiidomasno02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ex01
+            double num1 = 11.12;
+            double num2 = 5.62;
+            double num3 = num1 / num2;
+            Console.WriteLine(num3);
+            Console.ReadLine();
+
+            int num4 = 18;
+            int num5 = 9;
+            int num6 = num4 / num5;
+            Console.WriteLine(num6);
+            Console.ReadLine();
+            #endregion
+
+            #region Ex02
+            string str1 = "Dime ";
+            string str2 = "Kastro";
+            string str3 = str1 + str2;
+            Console.WriteLine(str3);
+            Console.ReadLine();
+
+            string str4 = "9";
+            string str5 = "3";
+            string str6 = str4 + str5;
+            Console.WriteLine(str6);
+            Console.ReadLine();
+            #endregion
+
+            #region Ex03
+            string str7 = "String and integer";
+            int num7 = 5;
+            var strAndInt = str7 + num7;
+            Console.WriteLine(strAndInt);
+            Console.ReadLine();
+            #endregion
+
+            #region Ex04
+            int n = 102;
+            int m = 5;
+            int result = n / m;
+            Console.WriteLine($"The number of SMS u can send is {result}.");
+            Console.ReadLine();
+            #endregion
+
+            #region Ex05BonusTask
+            //Set variable from input
+            //TRY to convert it to int
+            //if its converted successfully print in the console if number is odd or even
+            //else if its not converted, print in the console that user inserted invalid input
+            //Hint: if number / 2 has remainder 0 its even, else its odd
+            Console.WriteLine("vnesi broj:");
+
+            var input = Console.ReadLine();
+            int number1;
+            bool reulst = int.TryParse(input, out number1);
+            if (reulst && number1 % 2 == 0) 
+            {              
+                Console.WriteLine("user input is even");
+            }
+            else if (reulst && number1 % 2 == 1)
+            {
+                Console.WriteLine("user input is odd");
+            }
+            else
+            {
+                Console.WriteLine("KE PRAVISH SHO KE TI KAZAM!!");
+            }
+            #endregion
+
+            Console.WriteLine();
+            Console.WriteLine("TY! BB!");
+            Console.ReadLine();
+
+        }
+    }
+}
