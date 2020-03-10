9a727e6f59a9e0d1a1e591ad94ce5d77dd5a0d60
DimitarKastratovikj <61827749+DimitarKastratovikj@users.noreply.github.com>
2020-03-10 10:07:59 +0100

    vezbinet

--- Chas02/VezbiNet01/Program.cs
+++ Chas02/VezbiNet01/Program.cs
@@ -7,25 +7,25 @@ namespace VezbiNet01
         static void Main(string[] args)
         {
             #region 01
-            Console.WriteLine("1.Write a C# Sharp program to print Hello and your name in a separate line.");
+            Console.WriteLine("01.Write a C# Sharp program to print Hello and your name in a separate line.");
             Console.WriteLine();
             Console.WriteLine("Hellou:");
             Console.WriteLine("Alexandra Abramov");
             #endregion
 
             #region 02
-            Console.WriteLine("2.Write a C# Sharp program to print the sum of two numbers.");
+            Console.WriteLine("02.Write a C# Sharp program to print the sum of two numbers.");
             Console.WriteLine(5 + 6);
             #endregion
 
             #region 03
-            Console.WriteLine("3. Write a C# Sharp program to print the result of dividing two numbers.");
+            Console.WriteLine("03. Write a C# Sharp program to print the result of dividing two numbers.");
             Console.WriteLine(24 / 6);
             #endregion
 
             #region 04
             //4.Write a C# Sharp program to print the result of the specified operations
-            Console.WriteLine("4.Write a C# Sharp program to print the result of the specified operations");
+            Console.WriteLine("04.Write a C# Sharp program to print the result of the specified operations");
             Console.WriteLine(-1 + 4 * 6);
             Console.WriteLine((35 + 5) % 7);
             Console.WriteLine(14 + -4 * 6 / 11);
@@ -33,7 +33,7 @@ namespace VezbiNet01
             #endregion
 
             #region 05
-            Console.WriteLine("5. Write a C# Sharp program to swap two numbers");
+            Console.WriteLine("05. Write a C# Sharp program to swap two numbers");
             int num1, num2, tmp;
 
             Console.Write("\nInput the First Number : ");
@@ -54,7 +54,7 @@ namespace VezbiNet01
             #endregion
 
             #region 06
-            Console.WriteLine("Write a C# Sharp program to print the output of multiplication " +
+            Console.WriteLine("06.Write a C# Sharp program to print the output of multiplication " +
                 "of three numbers which will be entered by the user");
             Console.WriteLine();
             //Test Data:
@@ -76,6 +76,23 @@ namespace VezbiNet01
             Console.WriteLine($"Third number to multiply: {num5}");
             Console.WriteLine();
             Console.WriteLine($" Output: {num3 * num4 * num5}");
+
+            //resenie od net
+
+            //int num1, num2, num3;
+
+            //Console.Write("Input the first number to multiply: ");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Input the second number to multiply: ");
+            //num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Input the third number to multiply: ");
+            //num3 = Convert.ToInt32(Console.ReadLine());
+
+            //int result = num1 * num2 * num3;
+            //Console.WriteLine("Output: {0} x {1} x {2} = {3}", num1, num2, num3, result);
+
             Console.WriteLine();
             #endregion
 
@@ -93,10 +110,24 @@ namespace VezbiNet01
             Console.WriteLine($"First and Second number multiplying result is : {num6 * num7} ");
             Console.WriteLine($"First and Second number dividing result is : {num6 / num7} ");
             Console.WriteLine();
+
+                                                           //resenie od net 
+            //Console.Write("Enter a number: ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter another number: ");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            //Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+            //Console.WriteLine("{0} x {1} = {2}", num1, num2, num1 * num2);
+            //Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+            //Console.WriteLine("{0} mod {1} = {2}", num1, num2, num1 % num2);
+
             #endregion
 
             #region 08
-            Console.WriteLine("Write a C# Sharp program that takes a number as input" +
+            Console.WriteLine("08.Write a C# Sharp program that takes a number as input" +
                 " and print its multiplication table.");
             //Test Data:
             //Enter the number: 5
@@ -140,12 +171,56 @@ namespace VezbiNet01
             Console.WriteLine("             : {0} x {1} = {2}", x, 10, result1);
 
             #endregion
+
             #region 09
+            Console.WriteLine("09. Write a C# Sharp program that takes four numbers as input to calculate and print the average");
+            Console.WriteLine();
+
+            int num8, num9, num10, num11;
+
+            Console.Write("\nInput the 1st Number : ");
+            num8 = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the 2nd Number : ");
+            num9 = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the 3th Number : ");
+            num10 = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the 4th Number : ");
+            num11 = int.Parse(Console.ReadLine());
+
+            double result2 = (num8 + num9 + num10 + num11) / 4;
+            Console.WriteLine($"The average of {num8}, {num9}, {num10} and {num11} is {result2}");
+
+            //double result2 = (number1 + number2 + number3 + number4) / 4;  resenie od net
+            //Console.WriteLine("The average of {0}, {1}, {2}, {3} is: {4} ",number1, number2, number3, number4, result2);
 
             #endregion
+
             #region 10
+            Console.WriteLine("10. Write a C# Sharp program to that takes three numbers(x,y,z)" +
+                " as input and print the output of (x+y).z and x.y + y.z");
+            int num12, num13, num14;
+
+            Console.Write("Enter first number - ");
+            num12 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second  number - ");
+            num13 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter third  number - ");
+            num14 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"Result of specified numbers x = {num12},y = {num13} and z = {num14}, " +
+                $"(x+y)*z is {(num12 + num13)* num14} and x*y + y*z is {num12 * num13 + num13 * num14}");
+
             #endregion
+
             #region 11
+            Console.WriteLine("Write a C# Sharp program that takes an age (for example 20)" +
+                " as input and prints something as" + "You look older than 20");
+            Console.WriteLine();
+            int age;
+            Console.Write("Enter your age ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("You look younger than {0} ", age);
+            Console.WriteLine();
             #endregion
             #region 12
             #endregion
