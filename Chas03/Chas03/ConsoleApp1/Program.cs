a4ba7780e735574cb00f356e47d54d81cf7da656
DimitarKastratovikj <61827749+DimitarKastratovikj@users.noreply.github.com>
2020-03-12 16:06:47 +0100

    class3

---
+++ Chas03/Chas03/ConsoleApp1/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array.indexOf
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+            // we get the index of 2 in array
+            int indexOfTwo = Array.IndexOf(array, 2);
+            Console.WriteLine(indexOfTwo);
+
+            // we revert the array
+            Array.Reverse(array);
+
+            foreach (var number in array)
+            {
+                Console.WriteLine(number);
+            }
+
+            // we copy the array in another array
+            int[] clonedArray = new int[5];
+            Array.Copy(array, clonedArray, 5);
+
+            foreach (var number in clonedArray)
+            {
+                Console.WriteLine(number);
+            }
+
+            // sort the array ASC
+            Array.Sort(array);
+            foreach (var number in array)
+            {
+                Console.WriteLine(number);
+            }
+
+            // get the length of array
+            int length = array.Length;
+            Console.WriteLine(length);
+
+            // get the last index of element in array
+            int[] newArray = new int[] { 1, 2, 3, 1, 5, 6, 7, 8 };
+            int lastIndexOfOne = Array.LastIndexOf(newArray, 1);
+            Console.WriteLine(lastIndexOfOne);
+
+            //-----------------------------------------------
+
+            //Declare a new array of integers with 5 elements
+            int[] numbers = new int[5];
+
+            //initialize the array elements with values from input
+
+            // we use numberOfElement only for printing which element user is entering
+            int numberOfElements = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numberOfElements = i + 1;
+
+                Console.WriteLine("enter the " + numberOfElements + " number:");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            //sum all the values and print the result in the console
+            int suma = 0;
+
+            foreach (var num in numbers)
+            {
+                suma += num;
+            }
+
+            Console.WriteLine(suma);
+
+            //------------------------------------------------------
+            // DEFINITION 1
+            //Declaration
+            int[] numbers = new int[2];
+
+            //Assignment
+            numbers[0] = 10;
+            numbers[1] = 20;
+
+            // Note in this case we wont have compile time error but app will throw exception
+            numbers[3] = 100;
+
+            //DEFINITION 2
+            // Declare and init at same time
+            int[] nums = new int[2] { 10, 20 };
+
+            //Note in this case we will have compile time error. Gues why?
+            int[] errorNums = new int[2] { 10, 20 ,30 };
+
+            // DEFINITION 3
+            int[] numArray = new int[] { 10, 20, 30, 40, 50 };
+
+
+            //------------------------------------------------------------
+            // increment/decrement
+            int number = 1;
+
+            // we use it for checking if input from console is successfully converted
+            bool result;
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a number:");
+
+                result = int.TryParse(Console.ReadLine(), out number);
+
+                // checking if number is converted successfully
+                // if not we want to go to beggining ask the user to enter a number
+                // note that if number is not converted all code after "continue" wont be executed
+                if (result == false)
+                    continue;
+
+                // this code will be executed if result == true which means user entered number
+                Console.WriteLine("Congratulations, you entered a number !");
+                break;
+
+            }
+
+            // This example is the same but implemented with for instead with while
+            // in this case we dont set condition which means that loop will be endless
+
+            int num;
+            bool res;
+            for (int i = 0; ; i++)
+            {
+                Console.WriteLine("Please enter a number:");
+
+                res = int.TryParse(Console.ReadLine(), out num);
+
+                if (res == false)
+                    continue;
+
+                Console.WriteLine("Congratulations, you entered a number");
+                break;
+            }
+            Console.ReadLine();
+            //---------------------------------------------------------------------
+            // SYNTAX
+
+
+            // FOR
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Print all even numbers to 100 with for
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+            }
+
+
+            // WHILE
+            int m = 0;
+
+            while (m < 10)
+            {
+                Console.WriteLine(m);
+                m++;
+            }
+
+            //Print all even numbers to 100 with while
+            int increment = 1;
+            while (increment <= 100)
+            {
+                if (increment % 2 == 0)
+                    Console.WriteLine(increment);
+
+                increment++;
+            }
+
+
+            // DO WHILE
+            int n = 0;
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+
+            } while (n < 10);
+
+            //Print all even numbers to 100 with do while
+            int i = 1;
+            do
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+
+                i++;
+            } while (i <= 100);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chas03/domasna03/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace domasna03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////            EXERCISE 01:
+
+            //Get an input number from the console
+            //Print all numbers from 1 to that number
+            //Get another input number from the console
+            //Print all numbers from that number to 1
+
+            //Console.Write("enter a number ++> ");
+            //var userInput = Console.ReadLine();
+            //int validNumber;
+            //bool validInput = int.TryParse(userInput, out validNumber);
+
+            //if (validInput)
+            //{
+            //    for (int i = 1; i <= validNumber; i++)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //Console.Write("enter a number <-- ");
+            //var userInput1 = Console.ReadLine();
+            //int validNumber1;
+            //bool validInput1 = int.TryParse(userInput1, out validNumber1);
+
+            //if (validInput1)
+            //{
+            //    for (int i = validNumber1; i >= 1; i--)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            ////            EXERCISE 02:
+
+            ////Get an input number from the console
+            ////Print all even numbers starting from 2
+            ////Get another input number from the console
+            ////Print all odd numbers starting from 1
+
+            //Console.Write("enter a number <even> ");
+            //var userInput2 = Console.ReadLine();
+            //int validNumber2;
+            //bool validInput2 = int.TryParse(userInput2, out validNumber2);
+
+            //if (validInput2)
+            //{
+            //    for (int i = 1; i <= validNumber2; i++)
+            //    {
+            //        if (i % 2 == 0)
+            //            Console.WriteLine(i);
+            //    }
+            //}
+
+            //Console.Write("enter a number <odd> ");
+            //var userInput3 = Console.ReadLine();
+            //int validNumber3;
+            //bool validInput3 = int.TryParse(userInput3, out validNumber3);
+
+            //if (validInput3)
+            //{
+            //    for (int i = 0; i <= validNumber3; i++)
+            //    {
+            //        if (i % 2 == 1)
+            //            Console.WriteLine(i);
+            //    }
+            //}
+
+            //            EXERCISE 03:
+
+            //Create an array with names
+            //Give an option to the user to enter a name from the keyboard
+            //After entering the name put it in the array
+            //Ask the user if they want to enter another name(Y / N)
+            //Do the same process over and over until the user enters N
+            //Print all the names after user enters N
+            //Hint: we need to use endless loop until user enter N, then we want to "break" the loop
+
+
+            string[] strNames = new string[] { "Dime", "Bocka", "Ogi" };
+            Console.Write("Enter a name in the list: ");
+            var addName = Console.ReadLine();
+            int fInput;
+            string trueString;
+            bool falceImp = int.TryParse(addName,out fInput);
+
+            if (!falceImp)
+            {
+                trueString = addName;
+                Console.WriteLine($"You have added<{trueString}>to the list of names");
+                Array.Resize(ref strNames, 4);
+                strNames[3] = trueString;
+                Console.WriteLine();
+
+            }
+            else
+            {
+                Console.WriteLine("Enater a Valid name!");
+            }
+            
+            foreach (var item in strNames)
+            {
+                Console.WriteLine();
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadLine();
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write("Enter a number: ");
+            //    string input = Console.ReadLine();
+            //    if (!int.TryParse(input, out array[i])
+            //    {
+            //        // Tell the user something went wrong:
+            //        Console.WriteLine("Sorry, '{0}' is not a valid number.", input);
+
+            //        // Don't move to the next array element:
+            //        i--;
+            //    }
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
