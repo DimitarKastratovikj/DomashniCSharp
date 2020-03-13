e2dd836f7dda513a7842a6db13a3454689b19d1e
DimitarKastratovikj <61827749+DimitarKastratovikj@users.noreply.github.com>
2020-03-13 10:37:37 +0100

    d3 so bugs ,raboti

--- Chas03/domasna03/Program.cs
+++ Chas03/domasna03/Program.cs
@@ -13,31 +13,31 @@ namespace domasna03
             //Get another input number from the console
             //Print all numbers from that number to 1
 
-            //Console.Write("enter a number ++> ");
-            //var userInput = Console.ReadLine();
-            //int validNumber;
-            //bool validInput = int.TryParse(userInput, out validNumber);
+            Console.Write("enter a number ++> ");
+            var userInput = Console.ReadLine();
+            int validNumber;
+            bool validInput = int.TryParse(userInput, out validNumber);
 
-            //if (validInput)
-            //{
-            //    for (int i = 1; i <= validNumber; i++)
-            //    {
-            //        Console.WriteLine(i);
-            //    }
-            //}
+            if (validInput)
+            {
+                for (int i = 1; i <= validNumber; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
 
-            //Console.Write("enter a number <-- ");
-            //var userInput1 = Console.ReadLine();
-            //int validNumber1;
-            //bool validInput1 = int.TryParse(userInput1, out validNumber1);
+            Console.Write("enter a number <-- ");
+            var userInput1 = Console.ReadLine();
+            int validNumber1;
+            bool validInput1 = int.TryParse(userInput1, out validNumber1);
 
-            //if (validInput1)
-            //{
-            //    for (int i = validNumber1; i >= 1; i--)
-            //    {
-            //        Console.WriteLine(i);
-            //    }
-            //}
+            if (validInput1)
+            {
+                for (int i = validNumber1; i >= 1; i--)
+                {
+                    Console.WriteLine(i);
+                }
+            }
 
             ////            EXERCISE 02:
 
@@ -46,33 +46,33 @@ namespace domasna03
             ////Get another input number from the console
             ////Print all odd numbers starting from 1
 
-            //Console.Write("enter a number <even> ");
-            //var userInput2 = Console.ReadLine();
-            //int validNumber2;
-            //bool validInput2 = int.TryParse(userInput2, out validNumber2);
+            Console.Write("enter a number <even> ");
+            var userInput2 = Console.ReadLine();
+            int validNumber2;
+            bool validInput2 = int.TryParse(userInput2, out validNumber2);
 
-            //if (validInput2)
-            //{
-            //    for (int i = 1; i <= validNumber2; i++)
-            //    {
-            //        if (i % 2 == 0)
-            //            Console.WriteLine(i);
-            //    }
-            //}
+            if (validInput2)
+            {
+                for (int i = 1; i <= validNumber2; i++)
+                {
+                    if (i % 2 == 0)
+                        Console.WriteLine(i);
+                }
+            }
 
-            //Console.Write("enter a number <odd> ");
-            //var userInput3 = Console.ReadLine();
-            //int validNumber3;
-            //bool validInput3 = int.TryParse(userInput3, out validNumber3);
+            Console.Write("enter a number <odd> ");
+            var userInput3 = Console.ReadLine();
+            int validNumber3;
+            bool validInput3 = int.TryParse(userInput3, out validNumber3);
 
-            //if (validInput3)
-            //{
-            //    for (int i = 0; i <= validNumber3; i++)
-            //    {
-            //        if (i % 2 == 1)
-            //            Console.WriteLine(i);
-            //    }
-            //}
+            if (validInput3)
+            {
+                for (int i = 0; i <= validNumber3; i++)
+                {
+                    if (i % 2 == 1)
+                        Console.WriteLine(i);
+                }
+            }
 
             //            EXERCISE 03:
 
@@ -94,7 +94,7 @@ namespace domasna03
 
             bool falceImp = int.TryParse(addName, out fInput);
             string yes1 = "y";
-            string no1 = "n";
+            
 
             int vnesiKocka = strNames.Length + 1;
             int vnesiRed = strNames.Length;
@@ -104,16 +104,16 @@ namespace domasna03
             Console.WriteLine($"You have added <{trueString}> to the list of names");
             Array.Resize(ref strNames, vnesiKocka);
             strNames[vnesiRed] = trueString;
-
-            Console.WriteLine("Do want to enter another name(Y / N)  11111");
+            Console.WriteLine();
+            Console.Write("Do want to enter another name(Y / N) ");
+           
             var yesNo = Console.ReadLine();
             string primer = yesNo;
 
-            while (primer == yes1) 
+            while (!falceImp && primer == yes1) 
             {
-               
                 string ime;
-                Console.Write("Enter a name in the list:  2222 ");
+                Console.Write("Enter a name in the list:  ");
                 var novoIme = Console.ReadLine();
                 ime = novoIme;
 
@@ -121,17 +121,13 @@ namespace domasna03
                 Array.Resize(ref strNames, vnesiKocka);
                 vnesiRed = vnesiRed + 1;
                 strNames[vnesiRed] = ime;
-
-                Console.WriteLine("Do want to enter another name(Y / N)  22");
+                Console.WriteLine();
+                Console.Write("Do want to enter another name(Y / N) : ");
+                
                 primer = Console.ReadLine();
             }
 
-
-
-            //else
-            //{
-            //    Console.WriteLine("Enater a Valid name!");
-            //}
+            Console.WriteLine();
             Console.WriteLine( "THE LIST OF NAMES : ");
             foreach (var item in strNames)
             {
@@ -142,21 +138,6 @@ namespace domasna03
 
             Console.ReadLine();
 
-            //for (int i = 0; i < array.Length; i++)
-            //{
-            //    Console.Write("Enter a number: ");
-            //    string input = Console.ReadLine();
-            //    if (!int.TryParse(input, out array[i])
-            //    {
-            //        // Tell the user something went wrong:
-            //        Console.WriteLine("Sorry, '{0}' is not a valid number.", input);
-
-            //        // Don't move to the next array element:
-            //        i--;
-            //    }
-            //}
-
-            Console.ReadLine();
         }
     }
 }
