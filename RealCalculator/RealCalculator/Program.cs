a803570316bc2c669d0711c8981e8a052564b703
DimitarKastratovikj <61827749+DimitarKastratovikj@users.noreply.github.com>
2020-04-03 16:35:35 +0200

    calc

--- RealCalculator/RealCalculator/Program.cs
+++ RealCalculator/RealCalculator/Program.cs
@@ -5,78 +5,93 @@ namespace RealCalculator
     class Program
     {
 
-        public static int Sum(int numOne, int numTwo)
+        public static int Sum(int n1, int n2)
         {
-            return numOne + numTwo;
+            return n2 + n1;
         }
 
-        public static int Substract(int numOne, int numTwo)
+        public static int Substract(int n1, int n2)
         {
-            return numOne - numTwo;
+            return n1 - n2;
         }
 
-        public static int Multiply(int numOne, int numTwo)
+        public static int Multiply(int n1, int n2)
         {
-            return numOne * numTwo;
+            return n1 * n2;
         }
 
-        public static int Divide(int numOne, int numTwo)
+        public static int Divide(int n1, int n2)
         {
-            return numOne / numTwo;
+            return n1 / n2;
         }
 
 
         static void Main(string[] args)
         {
-            //Console.ForegroundColor = ConsoleColor.Green;
+
             char[] operations = new char[] { '+', '-', '*', '/', 's', 'S' };
 
-
-            char operation, simbol;
-
+            int numberOne, numberTwo;
+            char simbol;
             bool validImputOne, validimputTwo, validChar;
+            string nOne, nTwo;
+            Console.WriteLine("Hellouu Real Calculator !");
 
-            Console.WriteLine("Hello Real Calculator !");
             while (true)
             {
-                int numberOne, numberTwo;
-
-                while (true)
+                Console.Write(" Pls enter a operatinon (+,-,/,*) or 's' to stop the calculator:  ");
+                validChar = char.TryParse(Console.ReadLine(), out simbol);
+                if (/*!validChar &&*/ Array.IndexOf(operations, simbol) != -1)
                 {
-                    Console.WriteLine(" Pls enter a operatinon (+,-,/,*) or 's' to stop the calculator:  ");
-                    validChar = char.TryParse(Console.ReadLine(), out simbol);
-
-                    if (validChar && Array.IndexOf(operations, simbol) != -1)
+                    while (validChar)
                     {
+                        Console.Write("Enter Number One : ");
+                        nOne = Console.ReadLine();
+                        validImputOne = int.TryParse(nOne, out numberOne);
+                        if (!validImputOne)
+                        {
+                            Console.WriteLine("Invalid input number");
+                        }
+
+                        Console.Write("Enter Number Two : ");
+                        nTwo = Console.ReadLine();
+                        validimputTwo = int.TryParse(nTwo, out numberTwo);
+                        if (!validimputTwo)
+                        {
+                            Console.WriteLine("Invalid input number");
+                        }
+
+                        switch (simbol)
+                        {
+                            case '+':
+                                int soberi = Sum(numberOne, numberTwo);
+                                Console.WriteLine($" the sum of {numberOne} and {numberTwo} is {soberi}");
+                                break;
+                            case '-':
+                                int minus = Substract(numberOne, numberTwo);
+                                Console.WriteLine($" the minus of {numberOne} and {numberTwo} is {minus}");
+                                break;
+                            case '*':
+                                int mnozi = Multiply(numberOne, numberTwo);
+                                Console.WriteLine($" the multipl of {numberOne} and {numberTwo} is {mnozi}");
+                                break;
+                            case '/':
+                                int deli = Divide(numberOne, numberTwo);
+                                Console.WriteLine($" the dev of {numberOne} and {numberTwo} is {deli}");
+                                break;
+                            default:
+                                Console.WriteLine("Invalid operation selected! The aplication will automatically close.");
+                                break;
+                        }
                         break;
                     }
-                    Console.WriteLine("Invalid operator!!!");
-                }
 
-                if (validChar && simbol == 's')
+                }
+                else
                 {
-                    Console.WriteLine("TY FOR USING THE CALCULATOR");
-                    break;
+                    Console.WriteLine("The aplication will automatically close........");
                 }
-                else if (validChar && simbol == 'S')
-                {
-                    Console.WriteLine("TY FOR USING THE CALCULATOR");
-                    break;
-                }
-                while (validChar)
-                {
-
-                }
-
-            }
-
-
-            Console.WriteLine("Enter Number One : ");
-
-            Console.WriteLine("Enter Number Two : ");
-
-
-            Console.ReadLine();
+            }     
         }
     }
 }
